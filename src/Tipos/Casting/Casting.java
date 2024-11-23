8138a87489ea85febbe969594a6c916d3afb0845
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-23 20:36:40 -0300

    Adicionando explicação de casting

--- src/Tipos/Casting/Casting.java
+++ src/Tipos/Casting/Casting.java
@@ -1,4 +1,15 @@
 package Tipos.Casting;
 
 public class Casting {
+    public static void main(String[] args) {
+
+        //Converte o numero de maneira forçada para caber na memória
+        double numero = (double) 100.0f;
+
+        //Irá remover Bits para que possa caber na memória, com isso o resultado será errado
+        short idade = (short) 100000L;
+
+        System.out.println(numero);
+        System.out.println(idade);
+    }
 }
