58788f8c028f199ca9726786a6cfbec3dc307ecf
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-24 16:16:19 -0300

    Arrays multidimensionais (Usando ForEach para exibição)

--- src/Estruturas/Arrays/Multidimensional.java
+++ src/Estruturas/Arrays/Multidimensional.java
@@ -3,23 +3,32 @@ package Estruturas.Arrays;
 public class Multidimensional {
     public static void main(String[] args) {
         //Array de arrays = [[0,2], [0,3], [0,4]]
-        int[][] meses = new int[12][31];
-        meses[0][0] = 1;
-        meses[0][1] = 2;
-        meses[0][2] = 3;
-        meses[0][3] = 4;
+        int[][] numeros = new int[2][4];
+        numeros[0][0] = 1;
+        numeros[0][1] = 2;
+        numeros[0][2] = 3;
+        numeros[0][3] = 4;
 
-        meses[1][0] = 1;
-        meses[1][1] = 2;
-        meses[1][2] = 3;
-        meses[1][3] = 4;
+        numeros[1][0] = 1;
+        numeros[1][1] = 2;
+        numeros[1][2] = 3;
+        numeros[1][3] = 4;
 
         //Irá trazer a localização do elemento em memória
-        System.out.println(meses[0]);
-        System.out.println(meses[1]);
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
 
         //Trara o valor guardado no local
-        System.out.println(meses[0][1]);
-        System.out.println(meses[1][2]);
+        System.out.println(numeros[0][1]);
+        System.out.println(numeros[1][2]);
+
+        System.out.println("=========================================");
+
+        //usando o ForEach para exibir os dados
+        for(int [] arrayNum : numeros){
+            for (int num : arrayNum){
+                System.out.println(num);
+            }
+        }
     }
 }
