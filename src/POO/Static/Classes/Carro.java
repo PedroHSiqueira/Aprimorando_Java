0377f5afdb6742c66c0a0049885121c711b05a45
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-26 18:32:11 -0300

    Métodos Estaticos

---
+++ src/POO/Static/Classes/Carro.java
@@ -0,0 +1,43 @@
+package POO.Static.Classes;
+
+public class Carro {
+    private String marca;
+    private double velocidadeMaxima;
+    private static double velocidadeLimite = 250;
+
+    public Carro(String marca, double velocidadeMaxima) {
+        this.marca = marca;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime(){
+        System.out.println("==================");
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Velocidade Final: " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite: " + Carro.velocidadeLimite);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+}
---
+++ src/POO/Static/Testes/CarroTeste.java
@@ -0,0 +1,16 @@
+package POO.Static.Testes;
+
+import POO.Static.Classes.Carro;
+
+public class CarroTeste {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Porsche", 280);
+        Carro carro2 = new Carro("BMW", 275);
+        Carro carro3 = new Carro("Mercedes", 270);
+
+        carro1.setVelocidadeLimite(180);
+        carro1.imprime();
+        carro2.imprime();
+        carro3.imprime();
+    }
+}
