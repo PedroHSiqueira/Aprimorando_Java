e68360b1190b7aace35594568b325c82dcf402ce
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-28 19:22:47 -0300

    Enumerates

---
+++ src/Estruturas/Enums/Dias.java
@@ -0,0 +1,11 @@
+package Estruturas.Enums;
+
+public enum Dias {
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
+}
---
+++ src/Estruturas/Enums/Enums.java
@@ -0,0 +1,10 @@
+package Estruturas.Enums;
+
+public class Enums {
+    public static void main(String[] args) {
+        //Seleciona apartir do Enum
+        Dias primeiroDia = Dias.DOMINGO;
+
+        System.out.println(primeiroDia);
+    }
+}
